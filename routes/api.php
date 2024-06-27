aac4449c3f88e225f524ad16fa3288286c48a045
“CaesarBourne” <caesaradek@gmail.com>
2024-06-27 22:48:36 +0100

    incorporarted new cors config for laravel 11

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,11 @@
+<?php
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
 Route::post('/login', function (Request $request) {
     $data = $request->validate([
