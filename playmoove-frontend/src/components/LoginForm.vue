e0972f0d0297d7d41271bc8a26ca56f0824c6f1b
“CaesarBourne” <caesaradek@gmail.com>
2024-07-04 23:44:43 +0100

    new changes for Vue login interface and email to call apis

--- playmoove-frontend/src/components/LoginForm.vue
+++ playmoove-frontend/src/components/LoginForm.vue
@@ -7,10 +7,10 @@
       <form @submit.prevent="submitForm" class="mt-8 space-y-6">
         <div class="rounded-md shadow-sm -space-y-px">
           <div>
-            <label for="username" class="sr-only">Username</label>
-            <input v-model="username" id="username" name="username" type="text" required
+            <label for="email" class="sr-only">Email</label>
+            <input v-model="email" id="email" name="email" type="email" required
                    class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                   placeholder="Username">
+                   placeholder="Email">
           </div>
           <div>
             <label for="password" class="sr-only">Password</label>
@@ -24,7 +24,6 @@
           <button type="submit"
                   class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
             <span class="absolute left-0 inset-y-0 flex items-center pl-3">
-
               <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg"
                    viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                 <path fill-rule="evenodd"
@@ -47,7 +46,7 @@ import axios from 'axios';
 export default {
   data() {
     return {
-      username: '',
+      email: '',
       password: ''
     }
   },
@@ -55,7 +54,7 @@ export default {
     async submitForm() {
       try {
         const response = await axios.post('http://localhost:8000/api/login', {
-          username: this.username,
+          email: this.email,
           password: this.password
         });
         
