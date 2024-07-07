1f51d13b6712da9a60cdfd1813d8223a751c1889
“CaesarBourne” <caesaradek@gmail.com>
2024-07-07 22:40:03 +0100

    updated changes for docer running port and ip for laravel comtainer

--- playmoove-frontend/src/main.js
+++ playmoove-frontend/src/main.js
@@ -2,6 +2,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./index.css";
 import router from "./router";
+import axios from "axios";
+
 // import "./assets/tailwind.css";
+axios.defaults.baseURL = "http://localhost:8000";
 
 createApp(App).use(router).mount("#app");
